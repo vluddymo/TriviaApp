20592e686dceffe3c457d730a81d2851e807eac6
vluddymo <wladi.moser@gmail.com>
2022-02-13 17:46:23 +0100

    Fix eventlistener handling

--- app.js
+++ app.js
@@ -3,24 +3,23 @@ const scoreDisplay = document.querySelector("#score");
 let score = 0;
 
 const genres = [
-    {
-        name: "film",
-        id: 11
-    },
-    {
-        name: "video games",
-        id: 15
-    },
-    {
-        name: "nature",
-        id: 17
-    },
-    {
-        name: "music",
-        id: 12
-    }
+  {
+    name: "film",
+    id: 11,
+  },
+  {
+    name: "video games",
+    id: 15,
+  },
+  {
+    name: "nature",
+    id: 17,
+  },
+  {
+    name: "music",
+    id: 12,
+  },
 ];
-
 
 const levels = ["easy", "medium", "hard"];
 
@@ -52,68 +51,75 @@ function addGenre(genre) {
     )
       .then((response) => response.json())
       .then((data) => {
-          card.setAttribute('data-question', data.results[0].question);
-          card.setAttribute('data-answer', data.results[0].correct_answer);
-          card.setAttribute('data-value', card.getInnerHTML());
-          console.log(data)
-        })
-        .then(done => card.addEventListener('click', flipCard))
-})
+        card.setAttribute("data-question", data.results[0].question);
+        card.setAttribute("data-answer", data.results[0].correct_answer);
+        card.setAttribute("data-value", card.getInnerHTML());
+        console.log(data);
+      })
+      .then((done) => card.addEventListener("click", flipCard));
+  });
 }
 
-genres.forEach(genre => {
-    addGenre(genre)
-})
+genres.forEach((genre) => {
+  addGenre(genre);
+});
 
-function flipCard(){
-    console.log("clicked");
-    this.style.fontSize = "15px";
-    this.innerHTML = "";
-    const textDisplay = document.createElement("div");
-    const trueButton = document.createElement("button");
-    const falseButton = document.createElement("button");
-    trueButton.classList.add("true-button")
-    falseButton.classList.add("false-button")
-    trueButton.innerHTML = "True";
-    falseButton.innerHTML = "False";
-    trueButton.addEventListener("click", getResult);
-    falseButton.addEventListener("click", getResult);
-    textDisplay.innerHTML = this.getAttribute("data-question");
- 
+function flipCard() {
+  console.log("clicked");
+  this.style.fontSize = "15px";
+  this.innerHTML = "";
+  const textDisplay = document.createElement("div");
+  const trueButton = document.createElement("button");
+  const falseButton = document.createElement("button");
+  trueButton.classList.add("true-button");
+  falseButton.classList.add("false-button");
+  trueButton.innerHTML = "True";
+  falseButton.innerHTML = "False";
+  trueButton.addEventListener("click", getResult);
+  falseButton.addEventListener("click", getResult);
+  textDisplay.innerHTML = this.getAttribute("data-question");
 
-    this.append(textDisplay, trueButton, falseButton);
+  this.append(textDisplay, trueButton, falseButton);
 
-    const allCards = Array.from(document.querySelectorAll(".card"));
-    allCards.forEach(card => card.removeEventListener("click", flipCard));
+  const allCards = Array.from(document.querySelectorAll(".card"));
+  allCards.forEach((card) => card.removeEventListener("click", flipCard));
 }
 
-function getResult(){
-    const cardOfButton = this.parentElement;
-    console.log(cardOfButton.getAttribute("data-answer"));
-    console.log(this.innerHTML)
-    if (cardOfButton.getAttribute("data-answer") === this.innerHTML) {
-        score = score + parseInt(cardOfButton.getAttribute("data-value"));
-        scoreDisplay.innerHTML = score;
-        cardOfButton.classList.add("correct-answer");
-        setTimeout(() => {
-            while (cardOfButton.firstChild){
-                cardOfButton.removeChild(cardOfButton.lastChild)
-            }
-            cardOfButton.innerHTML = cardOfButton.getAttribute("data-value");
-        }, 100)
-    } else {
-        cardOfButton.classList.add("wrong-answer");
-        setTimeout(() => {
-            while (cardOfButton.firstChild){
-                cardOfButton.removeChild(cardOfButton.lastChild)
-            }
-            cardOfButton.innerHTML = 0;
-        }, 100)
+function getResult() {
+    const parentCard = this.parentElement;
+    const result = parentCard.getAttribute("data-answer");
+    const answer = this.innerHTML;
+    console.log(result);
+    console.log(answer);
+  
+    while (parentCard.firstChild) {
+      parentCard.removeChild(parentCard.lastChild);
     }
-
+  
+    if (answer === result) {
+      parentCard.classList.add("correct-answer");
+      parentCard.innerHTML = parentCard.getAttribute("data-Value");
+      score = score + parseInt(parentCard.getAttribute("data-Value"));
+      scoreDisplay.innerHTML = score;
+    } else {
+      parentCard.classList.add("wrong-answer");
+      parentCard.innerHTML = 0;
+    }
+  
+    parentCard.classList.replace("card", "usedCard");
     const allCards = Array.from(document.querySelectorAll(".card"));
-    allCards.forEach(card => card.addEventListener("click", flipCard));
+    console.log(allCards.length);
+  
+    if (allCards.length > 0) {
+      allCards.forEach((card) => card.addEventListener("click", flipCard));
+      parentCard.removeEventListener("click", flipCard);
+    } else {
+      setTimeout(() => alert(`Congratulations! your Score is ${score}`), 100);
+      setTimeout(refresh, 200);
+    }
+  }
 
-    cardOfButton.removeEventListener("click", flipCard);
-
-}
+function refresh() {
+    window.location.reload();
+  }
+  
